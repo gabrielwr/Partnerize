848a3e1b94e307eba0a4e4396658603f0a8c30a4
Gabriel <gabriel.wrowe@gmail.com>
2017-10-04 13:23:39 -0700

    chore: delete dead code

--- app/components/Message.js
+++ app/components/Message.js
@@ -1,47 +1,4 @@
 
-// import * as firebase from 'firebase';
-
-// export class Message extends React.Component {
-
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       messages: [],
-//       userId: null
-//     }
-
-//     //class method binding
-//     this.onSend = this.onSend.bind(this);
-//   }
-
-//   onAvatarPress() {
-//     console.log('made it here')
-//     this.props.navigation.navigate('User');
-//   }
-
-//   onSend(messages = []) {
-//     this.setState( previousState => {
-//       return {
-//         messages: GiftedChat.append(previousState.messages, messages),
-//       }
-//     });
-//   }
-
-//   render() {
-
-//     return (
-//       <GiftedChat
-//         messages={this.state.messages}
-//         onSend={this.onSend}
-//         user={{
-//           _id: 1,
-//         }}
-//         onClick={this.onAvatarPress}
-//       />
-//     );
-//   }
-// }
-
 
 import React from 'react';
 // import io from 'socket.io-client/socket.io'
