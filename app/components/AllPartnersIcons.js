7baab6dd2ba0e6ebc7c8c90852badbaa92cea6e7
Gabriel <gabriel.wrowe@gmail.com>
2017-10-14 14:42:06 -0500

    feat: fix icon style

--- app/components/AllPartnersIcons.js
+++ app/components/AllPartnersIcons.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import {
   Container,
@@ -37,22 +37,28 @@ export class AllPartnersIcons extends React.Component {
   render() {
     const { personObj, navigate } = this.props
     return (
-      <View>
+      <View style={styles.view}>
         <Icon
           style={{ color: 'dodgerblue' }}
           name='person'
           onPress={ () => this.onUserPress(personObj) }
         />
-        <Right>
-          <Icon
-            style={{ color: 'dodgerblue' }}
-            name='chatbubbles'
-            onPress={ () => {
-              navigate('Message', { user: personObj })
-            }}
-          />
-        </Right>
+        <Icon
+          style={{ color: 'dodgerblue' }}
+          name='chatbubbles'
+          onPress={ () => {
+            navigate('Message', { user: personObj })
+          }}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  view: {
+    width: '30%',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
+})
