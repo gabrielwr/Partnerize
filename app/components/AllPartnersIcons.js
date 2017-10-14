e0df31da7b976c128c3f6716c3b0b88e8fd4b7ca
Gabriel <gabriel.wrowe@gmail.com>
2017-10-14 14:07:04 -0500

    feat: clean up partnerIcons Component

--- app/components/AllPartnersIcons.js
+++ app/components/AllPartnersIcons.js
@@ -15,25 +15,34 @@ import {
 } from 'native-base';
 
 
-export class AllPartnersIcons extends React.Component{
+export class AllPartnersIcons extends React.Component {
+
+  constructor(props) {
+    super(props)
+    console.log('checking props:', this.props)
+    this.navigate = this.props.navigate
+  }
+
+  onUserPress(personObj) {
+    this.navigate('User', {
+      user: {
+        Name: personObj.name,
+        'One Arm Pullups': personObj['One Arm Pullups'],
+        'Favorite Climbing Area': personObj['Favorite Climbing Area'],
+        Distance: personObj.distance + ' Miles'
+      }
+    })
+  }
+
   render() {
     const { personObj, navigate } = this.props
     return (
-        <View>
-          <Icon
-            style={{ color: 'dodgerblue' }}
-            name='person'
-            onPress={ () => {
-              navigate('User', {
-                user: {
-                  Name: personObj.name,
-                  'One Arm Pullups': personObj['One Arm Pullups'],
-                  'Favorite Climbing Area': personObj['Favorite Climbing Area'],
-                  Distance: personObj.distance + ' Miles'
-                }
-              })
-            }}
-          />
+      <View>
+        <Icon
+          style={{ color: 'dodgerblue' }}
+          name='person'
+          onPress={ () => this.onUserPress(personObj) }
+        />
         <Right>
           <Icon
             style={{ color: 'dodgerblue' }}
